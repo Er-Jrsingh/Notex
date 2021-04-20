37f2fce3ad8a11ecab9659ffe90e118407fcccaf
Jitesh Singh <Jitust98@gmail.com>
2021-04-20 15:03:48 +0000

    Adding ButterKnife Dependency

--- app/src/main/java/com/codingcrazz/notex/MainActivity.java
+++ app/src/main/java/com/codingcrazz/notex/MainActivity.java
@@ -1,14 +1,24 @@
 package com.codingcrazz.notex;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
+
+    @OnClick(R.id.floatingAcButton)
+    void floatingActionButton(){
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
     }
 }
