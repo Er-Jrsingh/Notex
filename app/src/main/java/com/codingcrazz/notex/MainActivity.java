d97bebd9af591f6d2a1180014407396b73733ab5
Jitesh Singh <Jitust98@gmail.com>
2021-04-20 16:05:16 +0000

    Migrated To Basic Activity & Adding Some Resources

--- app/src/main/java/com/codingcrazz/notex/MainActivity.java
+++ app/src/main/java/com/codingcrazz/notex/MainActivity.java
@@ -1,24 +1,50 @@
 package com.codingcrazz.notex;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.View;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    @OnClick(R.id.floatingAcButton)
-    void floatingActionButton(){
-
-    }
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.notes_recyclerview)
+    RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab_add_note);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Hola Snacky", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
         ButterKnife.bind(this);
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
+        mRecyclerView.hasFixedSize();
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
     }
 }
