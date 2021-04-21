5c7e61c76630a3dfccbabb0730bd51fe13122893
Jitesh Singh <Jitust98@gmail.com>
2021-04-21 11:09:55 +0000

    Add Note Editor Activity for Creating & Editing Notes

---
+++ app/src/main/java/com/codingcrazz/notex/EditorActivity.java
@@ -0,0 +1,23 @@
+package com.codingcrazz.notex;
+
+import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+
+public class EditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout toolBarLayout = findViewById(R.id.toolbar_layout);
+        toolBarLayout.setTitle(getTitle());
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
--- app/src/main/java/com/codingcrazz/notex/MainActivity.java
+++ app/src/main/java/com/codingcrazz/notex/MainActivity.java
@@ -1,20 +1,17 @@
 package com.codingcrazz.notex;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.View;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,21 +19,19 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.notes_recyclerview)
     RecyclerView mRecyclerView;
 
+    @SuppressLint("NonConstantResourceId")
+    @OnClick(R.id.fab_add_note)
+    void OnFabClicked() {
+        Intent intent = new Intent(MainActivity.this, EditorActivity.class);
+        startActivity(intent);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab_add_note);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Hola Snacky", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         ButterKnife.bind(this);
         initRecyclerView();
